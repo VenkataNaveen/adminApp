b2e085267db2616cb07071b386a4fa8d0ce97119
VenkataNaveen <naveen.dudipalli@gmail.com>
2021-10-19 16:32:50 +0530

    app added body-parser, cors

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorisation');
+    if(req.method==='OPTINS'){
+        res.header('Access-Control-Allow-Methods',"PUT,POST,GET,DELETE,PATCH");
+      return  res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products',productRoutes);
 app.use('/orders',ordersRoutes);
